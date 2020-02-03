9b57c05e4342e3727c41437867475a780b1422b6
ChristianPorras <chrisporras1@uchicago.edu>
2020-02-03 04:07:57 +0000

    finished intro to routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,53 @@
 class ApplicationController < ActionController::Base
+  def homepage
+    render({ :template => "game/homepage.html.erb"})
+  end
+
+  def play_rock
+    moves = ["rock", "paper","scissors"]
+
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "paper"
+      @outcome = "lost"
+    elsif @computer_move == "scissors"
+      @outcome = "won"
+    end
+
+    render({ :template => "game/rock.html.erb"})
+  end
+
+  def play_scissors
+    moves = ["rock", "paper","scissors"]
+
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "paper"
+      @outcome = "won"
+    elsif @computer_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game/scissors.html.erb"})
+  end
+
+  def play_paper
+    moves = ["rock", "paper","scissors"]
+
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tied"
+    elsif @computer_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game/paper.html.erb"})
+  end
 end
